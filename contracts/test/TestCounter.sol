043363ae5ccc8664a8026be5029d52c3b73705e0
Michael Montour <mmontour@enya.ai>
2024-05-22 23:17:37 -0700

    Adds a new test cases and fixes the initialization of the HybridAccount address.  Changes to be committed: 	modified:   contracts/test/TestCounter.sol

--- contracts/test/TestCounter.sol
+++ contracts/test/TestCounter.sol
@@ -11,16 +11,14 @@ import "../samples/HybridAccount.sol";
 contract TestCounter {
     mapping(address => uint256) public counters;
 
-    address payable immutable helperAddr;
+    address payable immutable demoAddr;
     
-    constructor(address payable _helperAddr) {
-      helperAddr = _helperAddr;
+    constructor(address payable _demoAddr) {
+      demoAddr = _demoAddr;
     }
-    
-    function count(uint32 a, uint32 b, address ha_addr) public {
-    
-       //HybridAccount HA = HybridAccount(helperAddr);
-       HybridAccount HA = HybridAccount(payable(ha_addr));
+
+    function count(uint32 a, uint32 b) public {
+       HybridAccount HA = HybridAccount(demoAddr);
        uint256 x;
        uint256 y;
        if (b == 0) {
@@ -68,5 +66,50 @@ contract TestCounter {
             offset++;
             xxx[offset] = i;
         }
+    }
+
+    /* This example is a word-guessing game. The user picks a four-letter word as their guess,
+       and pays for the number of entries they wish to purchase. This wager is added to a pool.
+       The offchain provider generates a random array of words and returns it as a string[]. If
+       the user's guess appears in the list returned from the server then they win the entire pool.
+
+       A boolean flag allows the user to cheat by guaranteeing that the word "frog" will appear
+       in the list.
+    */
+
+    event GameResult(address indexed caller,uint256 indexed win, uint256 indexed Pool);
+    uint256 public constant EntryCost = 2 gwei;
+    uint256 public Pool = 0;
+
+    function wordGuess(string calldata myGuess, bool cheat) public payable {
+        HybridAccount HA = HybridAccount(payable(demoAddr));
+        uint256 entries = msg.value / EntryCost;
+	require(entries > 0, "No entries purchased");
+	require(entries <= 100, "Excess payment");
+        Pool += msg.value;
+	require(bytes(myGuess).length == 4, "Game uses 4-letter words");
+
+        bytes memory req = abi.encodeWithSignature("ramble(uint256,bool)", entries, cheat);
+        bytes32 userKey = bytes32(abi.encode(msg.sender));
+        (uint32 error, bytes memory ret) = HA.CallOffchain(userKey, req);
+        if (error != 0) {
+            revert(string(ret));
+	}
+
+	uint256 win = 0;
+	string[] memory words = abi.decode(ret,(string[]));
+
+        for (uint256 i=0; i<words.length; i++) {
+	    if (keccak256(bytes(words[i])) == keccak256(bytes(myGuess))) {
+	        win = Pool;  // Safe if there's more than one match in the list
+	    }
+	}
+
+	if (win == Pool) {
+	    Pool = 0;
+	    (bool sent,) = msg.sender.call{value: win}("");
+	    require(sent, "Payment failed");
+	}
+	emit GameResult(msg.sender,win,Pool);
     }
 }
