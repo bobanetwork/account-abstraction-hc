bbb3a6be7a6af907600ea14eb3b38a857ec8845b
wsdt <kevin.riedl@wavect.io>
2024-06-06 11:17:46 +0200

    feat: add withdraw, receive and getBalance to contract

--- contracts/test/TestCaptcha.sol
+++ contracts/test/TestCaptcha.sol
@@ -1,16 +1,39 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.12;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../samples/HybridAccount.sol";
 
-contract TestCaptcha {
+contract TestCaptcha is Ownable {
     address payable immutable helperAddr;
+    IERC20 public token;
+
+    uint256 private constant SAFE_GAS_STIPEND = 6000;
 
     constructor(address payable _helperAddr) {
         helperAddr = _helperAddr;
     }
 
-    function verifycaptcha(string calldata userAddress, string calldata uuid, string calldata captchaInput) public returns (bool) {
+    event Withdraw(address receiver, uint256 nativeAmount);
+
+    receive() external payable {}
+
+    function withdraw(uint256 _nativeAmount) public onlyOwner {
+        (bool sent, ) = msg.sender.call{
+            gas: SAFE_GAS_STIPEND,
+            value: _nativeAmount
+        }("");
+        require(sent, "Failed to send native Ether");
+
+        emit Withdraw(msg.sender, _nativeAmount);
+    }
+
+    function verifycaptcha(
+        string calldata userAddress,
+        string calldata uuid,
+        string calldata captchaInput
+    ) public returns (bool) {
         HybridAccount ha = HybridAccount(helperAddr);
 
         bytes memory req = abi.encodeWithSignature(
@@ -29,5 +52,13 @@ contract TestCaptcha {
         bool isVerified;
         (isVerified) = abi.decode(ret, (bool));
         return isVerified;
+    }
+
+    function getBalances()
+        public
+        view
+        returns (uint256 nativeBalance)
+    {
+        nativeBalance = address(this).balance;
     }
 }
